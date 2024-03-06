3a052463dc1052710d9e97d28dd3ad07e3bcedd2
Forgunda <forgunda87@gmail.com>
2024-03-06 12:39:10 +0300

    v1.1

--- local/modules/sav/lib/leadlog.php
+++ local/modules/sav/lib/leadlog.php
@@ -7,12 +7,13 @@ use Bitrix\Crm;
 //Класс Книг
 class Leadlog
 {
+    const $months = [ 1 => 'Январь' , 'Февраль' , 'Март' , 'Апрель' , 'Май' , 'Июнь' , 'Июль' , 'Август' , 'Сентябрь' , 'Октябрь' , 'Ноябрь' , 'Декабрь' ];
 	public static function mklog($LeadID,$UserID,$dir)
 		/*Создает лог файл в указанной дирректории $dir, 
 			определяет и пишет кто удалил лид ($LeadID- ID удаляегомого Лида) по его $UserID.
 		*/
 	{
-		$months = array( 1 => 'Январь' , 'Февраль' , 'Март' , 'Апрель' , 'Май' , 'Июнь' , 'Июль' , 'Август' , 'Сентябрь' , 'Октябрь' , 'Ноябрь' , 'Декабрь' );
+
 		//Создадим папку
 		$mnth = mktime(0, 0, 0, date("m"), 1,  date("Y"));
 		$new_dir1=$dir."/".$months[date('n',$mnth)].date('Y',$mnth)."/";
@@ -20,7 +21,7 @@ class Leadlog
 			mkdir($new_dir1);
 		}
 		//Создадим папку
-		$new_dir=$new_dir1.date('d').date('m').date('Y').'/';
+		$new_dir=$new_dir1.date('dmY').'/';
 		if(!file_exists($new_dir)){
 			mkdir($new_dir);
 		}
@@ -34,30 +35,26 @@ class Leadlog
 		$rsUser = \Bitrix\Main\UserTable::getById($UserID);
 		$arUser = $rsUser->fetch();
 		$usr='Удалил-'.$arUser['ID'].'-'.$arUser['NAME'].' '.$arUser['LAST_NAME'];
-		$leadResult = \Bitrix\Crm\LeadTable::getList(array(
-			'select' => array("*"),
-			'filter' => array(
+		$leadResult = \Bitrix\Crm\LeadTable::getList([
+			'select' => ["*"],
+			'filter' => [
 				'=ID' => $LeadID,
-			),
-		));
-		while ($lead=$leadResult->fetch())
-		{
-			$new_str=$usr.' \n\r';
-			if($lead["ID"]==$LeadID){
-				foreach($lead as $key=>$ld)
-				{
-					$new_str.='['.$key.']=>'.$ld;
-				}
-				$ER.=file_put_contents($filename, PHP_EOL.$new_str, FILE_APPEND);
-			}
-		}
+			],
+		]);
+		$lead=$leadResult->fetch()
+        $new_str=$usr.' \n\r';
+        if($lead["ID"]==$LeadID){
+            foreach($lead as $key=>$ld)
+            {
+                $new_str.='['.$key.']=>'.$ld;
+            }
+            $ER.=file_put_contents($filename, PHP_EOL.$new_str, FILE_APPEND);
+        }
 		return true;
 	}
 	public static function arclogs($dir)
 	{ 
 		/*Делает архив за прошлый месяц. Формат искомой папки апрель2024*/
-		$months = array( 1 => 'Январь' , 'Февраль' , 'Март' , 'Апрель' , 'Май' , 'Июнь' , 'Июль' , 'Август' , 'Сентябрь' , 'Октябрь' , 'Ноябрь' , 'Декабрь' );
-		//Создадим папку
 		$mnth = mktime(0, 0, 0, date("m")-1, 1,  date("Y"));
 		$new_dir=$dir.'/'.$months[date("n",$mnth)].date("Y",$mnth);
 		//создадим архив
